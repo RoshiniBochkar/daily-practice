fec88f51e75b3b38dc5300bfc3f48c1230d49394
RoshiniBochkar <roshinibochkar@gmail.com>
2026-08-21 12:32:08 +0530

    my practices

---
+++ rightangledtriangle.java
@@ -0,0 +1,10 @@
+public class rightangledtriangle {
+    public static void main(String z[]){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
